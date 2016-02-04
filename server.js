8b58ff0a42c05c14ec10a3891871d68583598481
whs465 <whs465@gmail.com>
2016-02-04 17:25:43 -0500

    UPDATE implemented

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var PORT = process.env.PORT || 3000;
 
 var todos = [];
 var todoNextId = 1;
-
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
@@ -64,6 +63,37 @@ app.delete('/todos/:id', function(req, res) {
     res.json(matchedTodo);
   }
 });
+
+// PUT  /todos/:id
+app.put('/todos/:id', function(req, res) {
+  var body = _.pick(req.body, 'description', 'completed');
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {
+    id: todoId
+  });
+  var validAttributes = {};
+
+  if (!matchedTodo) {
+    return res.status(404).send();
+  }
+
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
+    return res.status(400).send();
+  }
+
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
+  }
+
+  _.extend(matchedTodo, validAttributes);
+  res.json(matchedTodo);
+
+});
+
 app.listen(PORT, function() {
   console.log('express listening in port ' + PORT + '!');
 });
