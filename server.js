e5673e01f491e6273667c8447e3e4c3ea72aa8f7
whs465 <whs465@gmail.com>
2016-02-04 16:13:32 -0500

    deleted API implemented

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var bodyParser=require('body-parser');
-var _=require('underscore');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -48,6 +48,22 @@ app.post('/todos', function(req, res) {
   res.json(body);
 });
 
+// DELETE /todos/:id
+
+app.delete('/todos/:id', function(req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {
+    id: todoId
+  });
+  if (!matchedTodo) {
+    res.status(404).json({
+      "error": "no todo found with that id"
+    });
+  } else {
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  }
+});
 app.listen(PORT, function() {
   console.log('express listening in port ' + PORT + '!');
 });
