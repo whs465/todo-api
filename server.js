7f20f96c9963405b8e27c909db8e2ca20a1c1a5e
whs465 <whs465@gmail.com>
2016-02-04 15:22:12 -0500

    refacto API wit underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser=require('body-parser');
+var _=require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -19,13 +21,10 @@ app.get('/todos', function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo;
-
-  todos.forEach(function(todo) {
-    if (todoId === todo.id) {
-      matchedTodo = todo;
-    }
+  var matchedTodo = _.findWhere(todos, {
+    id: todoId
   });
+
   if (matchedTodo) {
     res.json(matchedTodo);
   } else {
@@ -35,7 +34,13 @@ app.get('/todos/:id', function(req, res) {
 
 //POST /todos
 app.post('/todos', function(req, res) {
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
+
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+  body.description = body.description.trim();
+
   body.id = todoNextId++;
 
   todos.push(body);
