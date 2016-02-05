b41b75bd469d439c3ee0a81a9f24246205ae64c8
whs465 <whs465@gmail.com>
2016-02-04 21:16:37 -0500

    filter and searching

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res) {
 app.get('/todos', function(req, res) {
   var queryParams = req.query;
   var filteredTodos = todos;
-console.log(queryParams);
+  console.log(queryParams);
   if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
     filteredTodos = _.where(filteredTodos, {
       completed: true
@@ -25,6 +25,12 @@ console.log(queryParams);
   } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
     filteredTodos = _.where(filteredTodos, {
       completed: false
+    });
+  }
+
+  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    filteredTodos = _.filter(filteredTodos, function(todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
     });
   }
 
