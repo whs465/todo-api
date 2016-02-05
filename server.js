34149ff907d32370bd79d9867fdd225e1e37be0a
whs465 <whs465@gmail.com>
2016-02-04 20:47:20 -0500

    add completed filter

--- server.js
+++ server.js
@@ -15,7 +15,20 @@ app.get('/', function(req, res) {
 
 //GET /todos
 app.get('/todos', function(req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+console.log(queryParams);
+  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    filteredTodos = _.where(filteredTodos, {
+      completed: true
+    });
+  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {
+      completed: false
+    });
+  }
+
+  res.json(filteredTodos);
 });
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
