18d87d33927a36c2ec3c403ada9020e1368211a3
whs465 <whs465@gmail.com>
2016-02-05 16:13:34 -0500

    convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -41,15 +41,15 @@ app.get('/todos', function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {
-        id: todoId
+    db.todo.findById(todoId).then(function(todo) {
+        if (!!todo) {
+            res.json(todo.toJSON());
+        } else {
+            res.status(404).send();
+        }
+    }, function(e) {
+        res.status(500).send();
     });
-
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
 });
 
 //POST /todos
